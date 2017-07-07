8d33296e2152d63b33f04f1522ad7db91b2f2116
Ashish Verma <ashish1500616@gmail.com>
2017-07-07 17:42:36 +0530

    add Recipe and Shopping Components.

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient{
+
+  // constructor(name:string , amount:number){
+  //   this.name=name;
+  //   this amount=amount;
+  // }
+  constructor(public name:string , public amount :number){}
+}
