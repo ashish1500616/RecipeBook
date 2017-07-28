000233e6a7cf4fbabf6aee042f9feb125f570f10
Ashish Verma <ashish1500616@gmail.com>
2017-07-28 22:31:44 +0530

    drop down Start working

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,14 +8,17 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient []=[
-  new Ingredient('Apples',5),
-  new Ingredient('Tomatoes',5),
-  new Ingredient('Potatoes',5),
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
